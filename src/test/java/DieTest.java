797528ca4546aa86323ccfeb30a4cc37cb2b6bd6
Oreneile Sejeso <sejesoo@gmail.com>
2022-02-24 02:00:19 +0200

    Added more tests to DieTest

--- src/test/java/DieTest.java
+++ src/test/java/DieTest.java
@@ -1,30 +1,55 @@
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class DieTest {
-
-    Die die;
-
+    Die die6;
+    Die die20;
     @Test
-    void rollDie6Test(){
-        die = new Die(6);
-        int testValue = die.roll();
-        assertEquals(testValue, die.value);
+    void sidesDieTest(){
+        die6 = new Die(6);
+        die20 = new Die(20);
+        assertAll(
+                () -> assertEquals(die6.sides,6),
+                () -> assertEquals(die20.sides,20)
+        );
     }
 
     @Test
-    void rollDie20Test(){
-        die = new Die(20);
-        int testValue = die.roll();
-        assertEquals(testValue, die.value);
+    void rollDieTest(){
+        die6 = new Die(6);
+        die20 = new Die(20);
+        assertAll(
+                () -> assertEquals(die6.roll(),die6.value),
+                () -> assertEquals(die20.roll(),die20.value)
+        );
     }
 
+    @Test
+    void numberOfSidesAndLengthOfArrayTest() {
+        IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            die6 = new Die(7,new int[]{1,-1,1,1,1,2});
+        }, "the number of sides doesn't match the length of the array of probabilities.");
+        Assertions.assertEquals("the number of sides doesn't match the length of the array of probabilities.", thrown.getMessage());
+
+    }
+
+    @Test
+    void probabilitiesHasNegetiveNumbertest() {
+        IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+           die6 = new Die(6,new int[]{-1,-1,1,1,1,2});
+        }, "negative probabilities not allowed");
+        Assertions.assertEquals("negative probabilities not allowed", thrown.getMessage());
+
+    }
+    
     @Test
     void rollDieProbabilitiesTest(){
-        die = new Die(6,new int[]{1,1,1,1,1,2});
-        int testValue = die.roll();
-        assertEquals(testValue, die.value);
+        die6 = new Die(6,new int[]{1,0,0,0,0,0});
+        int testValue = die6.roll();
+        assertEquals(testValue, die6.value);
     }
 }

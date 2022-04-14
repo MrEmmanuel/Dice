3c397cb751ef3b6f3e72b373a766875bbd7522c6
Oreneile Sejeso <sejesoo@gmail.com>
2022-04-14 02:02:42 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,13 @@ public class Main {
 
         Die die20 = new Die(20);
         die20.roll();
+        
+        Die dieDodgy6 = new Die(6,new int[]{0, 0, 0, 1, 0, 0});
+        dieDodgy6.roll();
 
         System.out.println(die6.value);
         System.out.println(die20.value);
+        System.out.println(dieDodgy6.value);
 
     }
 }
